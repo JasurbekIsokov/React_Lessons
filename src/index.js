3162477c45beef36232fe92d4829b108426531b0
Jasurbek <jasurisoqov685@gmail.com>
2022-04-20 09:11:37 +0500

    create CommentComponent import and export

--- src/index.js
+++ src/index.js
@@ -33,28 +33,16 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentComponent from "./CommentComponent.js";
 
 const App = function () {
   return (
-    <div className="ui comments">
-      <div className="comment">
-        <a className="avatar">
-          <img src="/images/avatar/small/stevie.jpg" />
-        </a>
-        <div className="content">
-          <a className="author">Stevie Feliciano</a>
-          <div className="metadata">
-            <div className="date">2 days ago</div>
-            <div className="rating">
-              <i className="star icon"></i>5 Faves
-            </div>
-          </div>
-          <div className="text">
-            Hey guys, I hope this example comment is helping you read this
-            documentation.
-          </div>
-        </div>
-      </div>
+    <div>
+      <CommentComponent />
+      <CommentComponent />
+      <CommentComponent />
+      <CommentComponent />
+      <CommentComponent />
     </div>
   );
 };
